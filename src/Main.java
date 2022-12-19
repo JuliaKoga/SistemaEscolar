2dece9200588500198648792c33006baca1f27cf
JuliaKoga <112908764+JuliaKoga@users.noreply.github.com>
2022-12-18 21:20:39 -0300

    adicionando método HorarioDeEntrada, que mostra se o aluno se atrasou ou não

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalTime;
 
 public class Main {
 
     public static void main(String[] args) {
         PessoaRepository cadastro = new PessoaRepository();
         Relatorio relatorio = new Relatorio();
+        HorarioDeEntrada entrada = new HorarioDeEntrada();
 
-        List<Materia> materia = new ArrayList<>();
+
+        List<Materia> materia1 = new ArrayList<>();
+        List<Materia> materia2 = new ArrayList<>();
 
         Endereco endereco1 = new Endereco.EnderecoBuilder()
                 .rua("Avenida Paulista")
@@ -52,15 +56,14 @@ public class Main {
         Aluno joao = new Aluno("João",16,"00000000-0",endereco1,Classe.B2);
         Aluno ana = new Aluno("Ana", 15,"00000000-0", endereco3,Classe.A1);
 
-        Professor luana = new Professor("Luana",31, "00000000-0",endereco2,"luana123@hotmail.com",materia);
-        Professor jose = new Professor("José",43,"00000000-00",endereco4,"jose123@live.com",materia);
+        Professor luana = new Professor("Luana",31, "00000000-0",endereco2,"luana123@hotmail.com",materia1);
+        Professor jose = new Professor("José",43,"00000000-00",endereco4,"jose123@live.com",materia2);
 
         Visitante carlos = new Visitante("Carlos",38,"00000000-0",endereco3);
 
-        materia.add(Materia.PORTUGUES);
-        materia.clear();
-        materia.add(Materia.FILOSOFIA);
-        materia.add(Materia.SOCIOLOGIA);
+        materia1.add(Materia.PORTUGUES);
+        materia2.add(Materia.FILOSOFIA);
+        materia2.add(Materia.SOCIOLOGIA);
 
         cadastro.cadastra(joao);
         cadastro.cadastra(luana);
@@ -69,6 +72,9 @@ public class Main {
         cadastro.mostrar();
 
         relatorio.relatorioUnico(carlos);
+
+        entrada.atraso(ana, LocalTime.parse("07:55:00"));
+        entrada.atraso(joao, LocalTime.parse("08:15:00"));
 
     }
 
